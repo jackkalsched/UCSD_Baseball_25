6710b8148aa709c19075d966e5eba95b27b6b274
Diego Osborn <diegoisaacosborn@gmail.com>
2025-08-22 12:41:13 -0700

    adding the csv files in a new directory

--- filepath.py
+++ filepath.py
@@ -5,12 +5,12 @@ pd.options.display.max_columns = None
 pd.options.display.max_rows = None
 pd.options.display.width = 0
 
-troot_folder = Path("Files")
+troot_folder = Path("Trackman CSV Files")
 troot_folder.mkdir(parents=True, exist_ok=True) 
 
-tcsv_game_files = [file for file in troot_folder.glob("*.csv") if not file.name.startswith("merged")]
+tcsv_game_files = [file for file in troot_folder.glob("*.csv") if not (file.name.startswith("merged") or file.name.startswith("pitch_class"))]
 if not tcsv_game_files:
-    raise FileNotFoundError("No CSV files found in the 'Files/' directory.")
+    raise FileNotFoundError("No CSV files found in the 'Trackman CSV Files/' directory.")
 
 trackman_games = pd.concat((pd.read_csv(file).drop(columns=['Notes'], errors='ignore') for file in tcsv_game_files), ignore_index=True)
 trackman_games = trackman_games.drop(columns=['Notes', 'Runner1st', 'Runner2nd', 'Runner3rd'], errors='ignore')
