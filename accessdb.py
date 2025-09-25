bc302d139e08a8511666a79e9896e46f92f0d009
Diego Osborn <diegoisaacosborn@gmail.com>
2025-09-25 01:52:51 -0700

    starting the class app process

--- accessdb.py
+++ accessdb.py
@@ -6,10 +6,16 @@ engine = create_engine("postgresql+psycopg2://buttz:Tritons1@localhost:5432/ucsd
 
 ## just change the necessary_cols to fit what exactly you're looking for
 ## be careful because every column is in the form of a str, so you have to change the type of the cols if need be after querying the data
-necessary_cols = ["TaggedPitchType", "AutoPitchType", 'RelSpeed']
-TEX = 'games'
+necessary_cols = ['Pitcher', 'PitcherThrows', 'PitcherTeam', 
+                  "TaggedPitchType", "AutoPitchType", 
+                  'RelSpeed', 'VertRelAngle', 'HorzRelAngle', 
+                  'SpinRate', 'SpinAxis', 'RelHeight', 'RelSide', 
+                  'Extension', 'InducedVertBreak', 'HorzBreak', 
+                  'VertApprAngle', 'HorzApprAngle'
+                  ]
+DATASET = 'games'
 
 cols_str = ", ".join([f'"{i}"' for i in necessary_cols])
-df = pd.read_sql(f'SELECT {cols_str} FROM {TEX};', engine)
-
+df = pd.read_sql(f'SELECT {cols_str} FROM {DATASET};', engine)
 print(df.head().dtypes)
+print(df)
